13877bb0607f3c34907fa439a868435c4b9692c9
remus00 <135813551+remus00@users.noreply.github.com>
2023-12-03 21:50:27 +0100

    Adding light and dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -13,8 +15,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
-            <html lang="en">
-                <body className={openSans.className}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+                <body className={cn(openSans.className, 'bg-white dark:bg-[#313338]')}>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="dark"
+                        enableSystem={false}
+                        storageKey="giscord-theme"
+                    >
+                        {children}
+                    </ThemeProvider>
+                </body>
             </html>
         </ClerkProvider>
     );
